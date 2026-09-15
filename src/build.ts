1aa710d2a630cd062ee5ef3881214529cbfbcf3c
RiaTheDreamer <reverie-relay@users.noreply.github.com>
2026-09-15 04:26:48 +0200

    Release 0.2.6 with valid Spindle version

--- src/build.ts
+++ src/build.ts
@@ -1,3 +1,3 @@
-export const EXTENSION_VERSION = '0.2.5.1'
+export const EXTENSION_VERSION = '0.2.6'
 export const BUILD_FAMILY = 'release'
-export const BUILD_ID = '20260915-0.2.5.1'
+export const BUILD_ID = '20260915-0.2.6'
