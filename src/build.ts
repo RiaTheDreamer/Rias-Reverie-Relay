bfa31d8e6154d89c427b1b0ffd578690b35a43a6
Ria <reverie-relay@users.noreply.github.com>
2026-09-25 12:21:52 +0200

    refine: smooth final image reveal motion

--- src/build.ts
+++ src/build.ts
@@ -1,3 +1,3 @@
-export const EXTENSION_VERSION = '0.2.8.7.21'
+export const EXTENSION_VERSION = '0.2.8.7.22'
 export const BUILD_FAMILY = 'release'
-export const BUILD_ID = '20260925-0.2.8.7.21'
+export const BUILD_ID = '20260925-0.2.8.7.22'
